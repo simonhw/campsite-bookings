a829faed71641c328e29dd9498c34dba4606c9ec
Simon Henleywillis <simonhenleywillis@gmail.com>
2024-05-18 15:15:42 +0000

    Feat: Upgrade admin dashboard with summernote.

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Booking
+from django_summernote.admin import SummernoteModelAdmin
+
+@admin.register(Booking)
+class BookingAdmin(SummernoteModelAdmin):
+
+    list_display = ('booked_by', 'arrival', 'accommodation')
+    search_fields = ['arrival']
+    list_filter = ('accommodation',)
 
 # Register your models here.
-admin.site.register(Booking)
