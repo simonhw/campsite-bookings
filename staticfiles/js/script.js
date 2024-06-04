aca812c61afa6677a85bd90d1443f6a554db4ebb
Simon Henleywillis <simonhenleywillis@gmail.com>
2024-06-04 13:57:12 +0000

    Feat: Update static files with js

---
+++ staticfiles/js/script.js
@@ -0,0 +1,18 @@
+// Add event listener to the arrival date input to calculate the minimim date for the departure input
+let arrivalInput = document.getElementById('arrival');
+let departureInput = document.getElementById('departure');
+
+arrivalInput.addEventListener('change', function() {
+    if (arrivalInput.value) { 
+        let arrivalDate = new Date(arrivalInput.value);
+        let oneDayAfter = new Date();
+
+        oneDayAfter.setDate(arrivalDate.getDate() + 1);
+
+        let year = oneDayAfter.getFullYear();
+        let month = ('0' + (oneDayAfter.getMonth() + 1)).slice(-2);
+        let day = ('0' + oneDayAfter.getDate()).slice(-2);
+        
+        departureInput.min = `${year}-${month}-${day}`;
+    }
+});
