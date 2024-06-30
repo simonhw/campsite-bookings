f4b6587c927df1eaac18a2dd9c9ce25fcb7c810c
Simon Henleywillis <simonhenleywillis@gmail.com>
2024-06-30 18:42:38 +0000

    Style: Remove commented-out code.

--- home/views.py
+++ home/views.py
@@ -1,11 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
 
-# Create your views here.
-# class HomePage(generic.DetailView):
-#     queryset = Booking.objects.all().order_by('arrival')
-#     template_name = 'home/index.html'
-
 def index_page(request):
     '''
     View that renders the index.html page.
