b338c9a53b3bbfe3cb82a03a143941cdc0a1e62c
Simon Henleywillis <simonhenleywillis@gmail.com>
2024-06-30 18:43:24 +0000

    Feat: Add Terms and Conditions required radio to form.

--- booking/forms.py
+++ booking/forms.py
@@ -1,6 +1,6 @@
 from .models import Booking
 from django import forms
-from django.forms.widgets import DateInput, SelectDateWidget, NumberInput
+from django.forms.widgets import DateInput, SelectDateWidget, NumberInput, RadioSelect
 from datetime import date, timedelta
 from django.core.exceptions import ValidationError
 
@@ -25,7 +25,11 @@ class BookingForm(forms.ModelForm):
             'departure',
             'adults',
             'children',
+            'booked',
         )
+        labels = {
+            'booked': 'I agree to the Terms and Conditions',
+        }
         widgets = {
             'arrival': DateInput(attrs={
                 'type': 'date',
@@ -53,6 +57,7 @@ class BookingForm(forms.ModelForm):
         cleaned_data = super().clean()
         arrival = cleaned_data.get('arrival')
         departure = cleaned_data.get('departure')
+        booked = cleaned_data.get('booked')
 
         if arrival > departure:
             raise ValidationError('Please select a departure date after your'
@@ -60,5 +65,8 @@ class BookingForm(forms.ModelForm):
 
         if arrival == departure:
             raise ValidationError('The minimum length of stay is one night.')
+        
+        if not booked:
+            raise ValidationError('You must accept the Terms and Conditions before booking.')
         
         return cleaned_data
