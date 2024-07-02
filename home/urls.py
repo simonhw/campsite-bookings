05805099cd17f7b21833be6d058bd650234ff2f6
Simon Henleywillis <simonhenleywillis@gmail.com>
2024-07-02 21:49:31 +0000

    Feat: Remove view to show 500 error page.

--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,6 @@
 from . import views
 from django.urls import path
 
-def homepage_view(request):
-    raise Exception("This is a test error")
-
 urlpatterns = [
     path('', views.index_page, name='home'),
-    path("500/", homepage_view),
 ]
