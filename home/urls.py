243053cdfe058d9080b8041ee3b2fbd5d42356fb
Simon Henleywillis <simonhenleywillis@gmail.com>
2024-07-03 19:59:12 +0000

    Fix: Add 500 page view for WAVE testing.

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,10 @@
 from . import views
 from django.urls import path
 
+def homepage_view(request):
+    raise Exception("This is a test error")
+
 urlpatterns = [
     path('', views.index_page, name='home'),
+    path("500/", homepage_view),
 ]
